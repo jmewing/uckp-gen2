fc71760135b46318d1a82cb0b9c2574502ce2949
Jeremy Ewing <jackflash219@gmail.com>
2022-05-21 15:59:01 -0500

    Update reinstall.sh

--- reinstall.sh
+++ reinstall.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Start first, by holding reset for 30 seconds and getting into recovery mode.
-
 # Once in recovery mode, update the device to version v0.8.6 (UCKP.apq8053.v0.8.6.8cf5792.181017.0942.bin)
 # This allows for more space on the original install/squish.fs locations
 # After updating to v0.8.6, reset to factory, then reboot.
